e0c13cf2579f1f3661fb591086a9b274ddba1673
Kirill Ivanov <kipwoker@users.noreply.github.com>
2021-12-01 10:16:10 +0100

    day1.part2

--- AoC2021/Day1.fs
+++ AoC2021/Day1.fs
@@ -1,7 +1,17 @@
 ﻿module Day1
-let solve (input: string[]) : string =
+let solve1 (input: string[]) : string =
     let numbers = input |> Array.map (fun x -> x |> int)
     let mutable result = 0
     for i = 1 to numbers.Length - 1 do
         if numbers.[i] > numbers.[i - 1] then result <- result + 1
+    result.ToString()
+
+let solve2 (input: string[]) : string =
+    let numbers = input |> Array.map (fun x -> x |> int)
+    let mutable sum = numbers.[0] + numbers.[1] + numbers.[2]
+    let mutable result = 0
+    for i = 3 to numbers.Length - 1 do
+        let newSum = sum - numbers.[i - 3] + numbers.[i]
+        if newSum > sum then result <- result + 1
+        sum <- newSum
     result.ToString()
--- AoC2021/Program.fs
+++ AoC2021/Program.fs
@@ -5,6 +5,6 @@ open System.IO
 [<EntryPoint>]
 let main argv =
     let input = File.ReadAllLines("input.txt", Encoding.UTF8)
-    let output = input |> Day1.solve
+    let output = input |> Day1.solve2
     printfn $"Output: %s{output}"
     0

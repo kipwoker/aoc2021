181afc26cb7f34da7a8047c60b944fb7165eac3b
Kirill Ivanov <kipwoker@users.noreply.github.com>
2021-12-05 13:08:57 +0100

    Day5. Fix to show both options

--- AoC2021/Day5.fs
+++ AoC2021/Day5.fs
@@ -35,10 +35,10 @@ let private getMax (lines: Line[]) : (int * int) =
 
 let private initField (maxX, maxY) = Array2D.init (maxX + 1) (maxY + 1) (fun _ _ -> 0)
 
-let private applyLines (field: int[,]) (lines: Line[]) : int[,] =
+let private applyLines (field: int[,]) (lines: Line[]) (diagonal: bool) : int[,] =
     lines |> Array.iter (fun line ->
-        match (line.End.X = line.Start.X, line.End.Y = line.Start.Y) with
-        | (true, _) ->
+        match (line.End.X = line.Start.X, line.End.Y = line.Start.Y, diagonal) with
+        | (true, _, _) ->
             let min' = min line.Start.Y line.End.Y
             let max' = max line.Start.Y line.End.Y
             let count = max' - min' + 1
@@ -49,7 +49,7 @@ let private applyLines (field: int[,]) (lines: Line[]) : int[,] =
                 let value = field.[x, y]
                 Array2D.set field x y (value + 1)
             )
-        | (_, true) ->
+        | (_, true, _) ->
             let min' = min line.Start.X line.End.X
             let max' = max line.Start.X line.End.X
             let count = max' - min' + 1
@@ -60,7 +60,7 @@ let private applyLines (field: int[,]) (lines: Line[]) : int[,] =
                 let value = field.[x, y]
                 Array2D.set field x y (value + 1)
             )
-        | _ ->
+        | (_, _, true) ->
             let minX = min line.Start.X line.End.X
             let maxX = max line.Start.X line.End.X
             let minY = min line.Start.Y line.End.Y
@@ -85,6 +85,7 @@ let private applyLines (field: int[,]) (lines: Line[]) : int[,] =
                 )
             else
                 ignore 0
+        | (false, false, false) -> ignore 0
     )
     field
 
@@ -94,11 +95,14 @@ let private count (field: int[,]) (moreThan: int) : int =
     |> Seq.filter (fun x -> x > moreThan)
     |> Seq.sumBy (fun _ -> 1)
 
-let solve1 (input: string[]) : string =
+let solve (input: string[]) (applyDiagonal: bool) : string =
     let lines = input |> parse
     let (maxX, maxY) =  lines |> getMax
     let field = initField (maxX, maxY)
-    let field' = applyLines field lines
+    let field' = applyLines field lines applyDiagonal
     printfn $"%A{field'}"
     let result = count field' 1
     result.ToString()
+
+let solve1 input = solve input false
+let solve2 input = solve input true
--- AoC2021/Program.fs
+++ AoC2021/Program.fs
@@ -4,6 +4,6 @@ open System.Text
 [<EntryPoint>]
 let main argv =
     let input = File.ReadAllLines("input5.txt", Encoding.UTF8)
-    let output = input |> Day5.solve1
+    let output = input |> Day5.solve2
     printfn $"Output: %s{output}"
     0

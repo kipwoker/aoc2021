8fc77509aeab6dcf7707696b151deae0772471c4
Kirill Ivanov <kipwoker@users.noreply.github.com>
2021-12-12 15:43:43 +0100

    Code compaction

--- AoC2021/Day12.fs
+++ AoC2021/Day12.fs
@@ -10,9 +10,7 @@ type Path = {
 }
 
 let parse (input : string[]) =
-    let directions =
-        input
-        |> Array.map (fun line ->
+    let directions = input |> Array.map (fun line ->
             let parts = line.Split('-')
             (parts.[0], parts.[1])
         )
@@ -27,47 +25,26 @@ let parse (input : string[]) =
 let traverseChildren routes node small maxSmall find' =
     routes
     |> Map.find node
-    |> Array.map (fun destination ->
-                        find' routes destination small maxSmall
-                        |> List.map (fun path ->
-                            {
-                                State = path.State
-                                Nodes = [node] @ path.Nodes
-                            })
-                  )
+    |> Array.map (fun destination -> find' routes destination small maxSmall |> List.map (fun path -> { State = path.State; Nodes = [node] @ path.Nodes }))
     |> Array.fold (fun state l -> state @ l) []
 
 let rec find (routes: Map<string, string[]>) (node: string) (small: Set<string>) (maxSmall: int) : Path list =
     match node with
-    | "start" ->
-        traverseChildren routes node small maxSmall find
-    | "end" ->
-        [{
-            State = Valid
-            Nodes = ["end"]
-        }]
+    | "start" -> traverseChildren routes node small maxSmall find
+    | "end" -> [{ State = Valid; Nodes = ["end"] }]
     | n when Char.IsLower(n.[0]) ->
         let containsSmall = small.Contains(n)
         match (containsSmall, maxSmall) with
-        | (true, 0) ->
-            [{
-                State = Invalid
-                Nodes = [n]
-            }]
-        | (true, _) ->
-            traverseChildren routes node (small.Add n) (maxSmall-1) find
-        | (false, _) ->
-            traverseChildren routes node (small.Add n) maxSmall find
-    | _ ->
-        traverseChildren routes node small maxSmall find
+        | (true, 0) -> [{ State = Invalid; Nodes = [n] }]
+        | (true, _) -> traverseChildren routes node (small.Add n) (maxSmall-1) find
+        | (false, _) -> traverseChildren routes node (small.Add n) maxSmall find
+    | _ -> traverseChildren routes node small maxSmall find
 
 
 let solve (input: string[]) (maxSmall: int) : string =
     let routes = input |> parse
-    let result = find routes "start" Set.empty maxSmall
-                 |> List.fold (fun state el ->
-                      state + (if el.State = Valid then 1 else 0)
-                    ) 0
+    let count = List.fold (fun state el -> state + (if el.State = Valid then 1 else 0)) 0
+    let result = find routes "start" Set.empty maxSmall |> count
     result.ToString()
 
 let solve1 input = solve input 0

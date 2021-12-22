80758481c9304672a7dafc6707f142b3801e824a
Kirill Ivanov <kipwoker@users.noreply.github.com>
2021-12-22 19:40:42 +0100

    Day22. 2nd part

--- AoC2021/Day22.fs
+++ AoC2021/Day22.fs
@@ -1,5 +1,6 @@
 ﻿module Day22
 
+open System.Numerics
 open Core
 
 type State = Off | On
@@ -9,11 +10,15 @@ type Range = {
     Max : int
 }
 
-type Command = {
-    State: State
+type Cuboid = {
     X : Range
     Y : Range
     Z : Range
+}
+
+type Command = {
+    State: State
+    Cuboid : Cuboid
 }
 
 let parseRange (x : string) =
@@ -28,39 +33,56 @@ let parse (input : string[]) =
                     | "on" -> On
                     | _ -> Off
         let ranges = parts.[1].Split(',') |> Array.map(fun x -> x.Split('=').[1] |> parseRange)
-        { State = state; X = ranges.[0]; Y = ranges.[1]; Z = ranges.[2] }
+        let cuboid = {X = ranges.[0]; Y = ranges.[1]; Z = ranges.[2]}
+        { State = state; Cuboid = cuboid }
     )
 
-let apply (commands: Command[]) (map : Map<int*int*int, State>) limit =
-    let rx,ry,rz = limit
-    commands
-    |> Array.fold (fun map' command ->
-        let xmin = max rx.Min command.X.Min
-        let ymin = max ry.Min command.Y.Min
-        let zmin = max rz.Min command.Z.Min
-        let xmax = min rx.Max command.X.Max
-        let ymax = min ry.Max command.Y.Max
-        let zmax = min rz.Max command.Z.Max
-        [xmin..xmax]
-        |> List.fold (fun mapx x ->
-            [ymin..ymax]
-            |> List.fold (fun mapy y ->
-                [zmin..zmax]
-                |> List.fold (fun mapz z ->
-                    mapz
-                    |> Map.change (x,y,z) (fun _ -> Some command.State)
-                ) mapy
-            ) mapx
-        ) map'
-    ) map
+let inline getLength range = if range.Max < range.Min then bigint 0 else bigint range.Max - bigint range.Min + bigint 1
+let inline count c =
+    let dx = getLength c.X
+    let dy = getLength c.Y
+    let dz = getLength c.Z
+    dx * dy * dz
 
-let calcOn (map : Map<int*int*int, State>) =
-    map |> Map.fold (fun sum _ state -> sum + (if state = On then 1 else 0)) 0
+let inline intersect (a: Cuboid) (b: Cuboid) : Cuboid option =
+    let cuboid = {
+                    X = { Min = max a.X.Min b.X.Min; Max = min a.X.Max b.X.Max }
+                    Y = { Min = max a.Y.Min b.Y.Min; Max = min a.Y.Max b.Y.Max }
+                    Z = { Min = max a.Z.Min b.Z.Min; Max = min a.Z.Max b.Z.Max }
+                 }
+    match count cuboid with
+    | z when z = bigint 0 -> None
+    | _ -> Some cuboid
+
+let inline inverse state = if state = On then Off else On
+let apply (commands: Command[]) =
+    commands
+    |> Array.fold (fun result a ->
+        let intersected = result
+                          |> List.map (fun (b : Command) -> (b.State, intersect a.Cuboid b.Cuboid))
+                          |> List.filter (fun (_,y) -> y.IsSome)
+                          |> List.map (fun (x,y) -> { State = inverse x; Cuboid = y.Value })
+        let lighted = if a.State = On then [a] else []
+        result @ intersected @ lighted
+    ) []
+
+let inline sign x = if x.State = On then bigint 1 else bigint -1
+let calcOn commands limit =
+    commands
+    |> List.fold(fun sum command ->
+        let cuboid = match limit with
+                     | Some l -> intersect command.Cuboid l
+                     | None -> command.Cuboid |> Some
+        match cuboid with
+        | Some cuboid' -> sum + count cuboid' * sign command
+        | None -> sum
+    ) BigInteger.Zero
 
 let solve input =
     let commands = input |> parse
-    let limit = ({Min = -50; Max = 50}, {Min = -50; Max = 50}, {Min = -50; Max = 50})
-    let map = apply commands Map.empty limit
-    let task1 = map |> calcOn
-    let result = task1
+    let limit = { X = {Min = -50; Max = 50}; Y = {Min = -50; Max = 50}; Z = {Min = -50; Max = 50}}
+    let commands' = apply commands
+    let task1 = calcOn commands' (Some limit)
+    let task2 = calcOn commands' None
+    let result = (task1, task2)
     result.ToString()
